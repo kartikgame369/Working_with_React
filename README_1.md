0e8f11ceaa075ddda0ca6461246b05cb4a2a9be7
kartikgame369 <kartikgame369doc@gmail.com>
2025-10-19 16:08:40 +0530

    new changes testing

---
+++ README_1.md
@@ -0,0 +1,38 @@
+# npm run start 
+# npm start
+# npm run build 
+# all these are written in pakage.json
+# but npx parcel index.html this is  different 
+//  const heading = React.createElement("h1",{id:"heading" ,xyz:"abc"},"hello world inside react!");
+//         const root = ReactDOM.createRoot(document.getElementById("root"));
+//         root.render(heading);
+
+// {} this object for attributes for tages{id:"heading"}
+// this returens me as an object 
+
+// paraent and clild class
+// reactelement(object )
+// at the end it will convert into html and show on the screen
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+
+// const paraent = React.createElement("div",
+//     {id:"paraent"},
+//     [
+//         React.createElement("div",{id:"child1"},
+//         [React.createElement("h1",{},"hi there is no change  "),
+//             React.createElement("h2",{},"hii its working")
+//         ]
+//     ),
+//     React.createElement("div",{id:"child2"},
+//         [React.createElement("h3",{},"i importated the react  "),
+//             React.createElement("h4",{},"type = module for error"),
+//         ]
+//     )
+//     ]
+
+// );
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(paraent);
+
+// // replaced but not appended[ root h1 = kartik is here ]
--- app.js
+++ app.js
@@ -1,33 +1,10 @@
-//  const heading = React.createElement("h1",{id:"heading" ,xyz:"abc"},"hello world inside react!");
-//         const root = ReactDOM.createRoot(document.getElementById("root"));
-//         root.render(heading);
 
-// {} this object for attributes for tages{id:"heading"}
-// this returens me as an object 
 
-// paraent and clild class
-// reactelement(object )
-// at the end it will convert into html and show on the screen
-import React from "react";
-import ReactDOM from "react-dom/client";
+import react from "react"
+import reactDOM from "react-dom/client"
 
-const paraent = React.createElement("div",
-    {id:"paraent"},
-    [
-        React.createElement("div",{id:"child1"},
-        [React.createElement("h1",{},"hi there is no change  "),
-            React.createElement("h2",{},"hii its working")
-        ]
-    ),
-    React.createElement("div",{id:"child2"},
-        [React.createElement("h3",{},"i importated the react  "),
-            React.createElement("h4",{},"type = module for error"),
-        ]
-    )
-    ]
+// React Element
 
-);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(paraent);
-
-// replaced but not appended[ root h1 = kartik is here ]
+const heading = React.createElement("h1",{id:"heading"},"hii its react 3part");
+const root = React.createRoot(document.getElementById("root"));
+root.render(heading)
