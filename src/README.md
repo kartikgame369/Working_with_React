fec2717d64f6b8e3ec059e4ca700542c65bfee80
kartikgame369 <kartikgame369doc@gmail.com>
2025-10-25 01:43:51 +0530

    just adding one function of filter the res-card

---
+++ src/README.md
@@ -0,0 +1,16 @@
+# state varible 
+# super powerfull variable 
+<!-- REACT hooks use state  -->
+<!-- they are normal js functions written by facebook devlopers  -->
+# useState()
+# useEffect()
+<!-- these two hooks are most important  -->
+is used to create state varibale it manintain the component
+step 1
+import {useState} from "react" # as a named import
+# createing the useState
+const {variable name } = useState([]);
+
+
+
+
--- src/components/Body.js
+++ src/components/Body.js
@@ -3,7 +3,13 @@ import reslist from "../utils/MockData";
 const Body =()=>{
   return(
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button className="filter-btn" onClick={()=>{
+          listOfRestro = listofrestro.filter(
+            (res)=>res.data.avgrating>4
+          );
+        }}>Top rated restorent </button>
+      </div>
       <div className="res-container">
         <RestarantCard  res-data={reslist} />
         <RestarantCard/>
